7a54bea81ca9dbf8951198925f81baba90480343
WahyuDwe <wdwihartarto@gmail.com>
2024-10-21 15:44:20 +0700

    set up for fleet

--- composeApp/src/commonMain/kotlin/org/dwe/mycomposemultiplatform/App.kt
+++ composeApp/src/commonMain/kotlin/org/dwe/mycomposemultiplatform/App.kt
@@ -4,27 +4,26 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import mycomposemultiplatform.composeapp.generated.resources.Res
 import mycomposemultiplatform.composeapp.generated.resources.compose_multiplatform
 import mycomposemultiplatform.composeapp.generated.resources.hello_world
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            OutlinedButton( onClick = { showContent = !showContent }) {
+            Button( onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
 
